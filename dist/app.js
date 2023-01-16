85510baa404efa5bf634eb39721b95b3478da56a
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-01-17 03:13:00 +0500

    I have completed section one for a text-based adventure game in which the user attacks a random enemy takes a potion and runs away from the dungeon.

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
 import chalkAnimation from "chalk-animation";
 import gradient from "gradient-string";
 import figlet from "figlet";
@@ -17,3 +19,62 @@ async function welcome() {
     rainbowTitle2.stop();
 }
 await welcome();
+// text-based adventure game variables 
+const enemies = ["Skeleton", "Warrior", "Assasin"];
+const maxEnemyHealth = 75;
+const enemyAttackDamage = 25;
+let health = 100;
+const attackDamage = 50;
+let numBandages = 3; // numHealthPotion  
+const numBandagesHealAmount = 30; // healthPotionHealAmount 
+const bandagesDropChance = 50; // healthPotionDorpChance
+let running = true;
+GAME: while (running) {
+    console.log(chalk.bold.red(`-------------------------------------------------------------------`));
+    let enemyHealth = Math.floor(Math.random() * maxEnemyHealth);
+    let enemy = enemies[Math.floor(Math.random() * enemies.length)];
+    chalkAnimation.pulse(`\t\t# ${enemy} is here #\n`);
+    await sleep();
+    while (enemyHealth > 0) {
+        console.log(chalk.green(`\t Your HP: ${health}`));
+        console.log(chalk.red(`\t ${enemy}'s HP: ${enemyHealth}`));
+        console.log(chalk.hex('#FFA500').bold(`\t What would you like to do?`));
+        let answer = await inquirer.prompt([
+            {
+                type: "list",
+                name: "choose",
+                choices: [
+                    chalk.yellow("Attack"),
+                    chalk.yellow("Bandage Wound"),
+                    chalk.yellow("Run"),
+                ],
+            },
+        ]);
+        if (answer.choose === chalk.yellow("Attack")) {
+            let damageDealt = Math.floor(Math.random() * attackDamage);
+            let damageTaken = Math.floor(Math.random() * enemyAttackDamage);
+            enemyHealth -= damageDealt;
+            health -= damageTaken;
+            console.log(chalk.hex('#ff4700').bold(`\t> You strike the ${enemy} for ${damageDealt} damage.`));
+            console.log(chalk.hex('#ff4700').bold(`\t> You recieve ${damageTaken} in retaliation!`));
+            if (health < 1) {
+                console.log(chalk.red(`You have taken too much damage, you are too weak to go on!`));
+                break;
+            }
+        }
+        else if (answer.choose == chalk.yellow("Bandage Wound")) {
+            if (numBandages > 0) {
+                health += numBandagesHealAmount;
+                numBandages--;
+                console.log(chalk.yellow(`You got a bandage \n You now have ${health} HP! \n You now have ${numBandages} left`));
+            }
+            else {
+                console.log(chalk.hex('#5ccd32').bold(`\t> You have no bandages left. Defeat enemies for a chance to get one.`));
+            }
+        }
+        else {
+            console.log(chalk.yellow(`You run away from the ${enemy}`));
+            continue GAME;
+        }
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -14,18 +14,18 @@ async function welcome() {
     `
     )
 
-  await sleep();
-  rainbowTitle.stop()
+    await sleep();
+    rainbowTitle.stop()
 
-  console.log(gradient.pastel.multiline(figlet.textSync("Adventure Game", { horizontalLayout: 'full' })))
+    console.log(gradient.pastel.multiline(figlet.textSync("Adventure Game", { horizontalLayout: 'full' })))
 
-  const rainbowTitle2 = chalkAnimation.rainbow(`
+    const rainbowTitle2 = chalkAnimation.rainbow(`
         \t \t Welcome to the DUNGEON!   
     `
     )
 
-  await sleep();
-  rainbowTitle2.stop()
+    await sleep();
+    rainbowTitle2.stop()
 }
 
 await welcome();
@@ -42,3 +42,78 @@ const numBandagesHealAmount: number = 30; // healthPotionHealAmount
 const bandagesDropChance: number = 50; // healthPotionDorpChance
 
 let running: boolean = true;
+
+GAME: while (running) {
+    console.log(
+        chalk.bold.red(
+            `-------------------------------------------------------------------`
+        )
+    );
+
+    let enemyHealth: number = Math.floor(Math.random() * maxEnemyHealth);
+    let enemy: string = enemies[Math.floor(Math.random() * enemies.length)];
+
+    chalkAnimation.pulse(`\t\t# ${enemy} is here #\n`);
+    await sleep();
+
+    while (enemyHealth > 0) {
+        console.log(chalk.green(`\t Your HP: ${health}`));
+        console.log(chalk.red(`\t ${enemy}'s HP: ${enemyHealth}`));
+        console.log(chalk.hex('#FFA500').bold(`\t What would you like to do?`));
+
+        let answer = await inquirer.prompt([
+            {
+                type: "list",
+                name: "choose",
+                choices: [
+                    chalk.yellow("Attack"),
+                    chalk.yellow("Bandage Wound"),
+                    chalk.yellow("Run"),
+                ],
+            },
+        ]);
+
+        if (answer.choose === chalk.yellow("Attack")) {
+            let damageDealt: number = Math.floor(Math.random() * attackDamage);
+            let damageTaken: number = Math.floor(Math.random() * enemyAttackDamage);
+
+            enemyHealth -= damageDealt;
+            health -= damageTaken;
+
+            console.log(
+                chalk.hex('#ff4700').bold(`\t> You strike the ${enemy} for ${damageDealt} damage.`)
+            );
+            console.log(
+                chalk.hex('#ff4700').bold(`\t> You recieve ${damageTaken} in retaliation!`)
+            );
+
+            if (health < 1) {
+                console.log(
+                    chalk.red(
+                        `You have taken too much damage, you are too weak to go on!`
+                    )
+                );
+                break;
+            }
+        } else if (answer.choose == chalk.yellow("Bandage Wound")) {
+            if (numBandages > 0) {
+              health += numBandagesHealAmount;
+              numBandages--;
+              console.log(
+                chalk.yellow(
+                  `You got a bandage \n You now have ${health} HP! \n You now have ${numBandages} left`
+                )
+              );
+            } else {
+              console.log(
+                chalk.hex('#5ccd32').bold(
+                  `\t> You have no bandages left. Defeat enemies for a chance to get one.`
+                )
+              );
+            }
+          } else {
+            console.log(chalk.yellow(`You run away from the ${enemy}`));
+            continue GAME;
+          }
+    }
+}
